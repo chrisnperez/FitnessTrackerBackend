6729d98c5dc69eb1a0993b97d0d6367981bbbde5
Chris Perez <chrisnaperez@gmail.com>
2023-06-01 15:57:10 -0700

    ran tests for users and passed

--- db/users.js
+++ db/users.js
@@ -1,24 +1,28 @@
 const client = require("./client");
 const bcrypt = require('bcrypt');
 
+
+
 // database functions
 
 // user functions
-async function createUser({ username, password }) {
+const createUser = async ({ username, password }) => {
 
   try {
 
     const SALT_COUNT = 10;
     const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
 
-    const {rows: [user] } = await client.query(`
+    const { rows: [user] } = await client.query(`
         INSERT INTO users(username, password) 
         VALUES ($1, $2)
         ON CONFLICT (username) DO NOTHING
         RETURNING *;
         `, [username, hashedPassword]);
 
-        return user;
+    delete user.password;
+
+    return user;
   }
   catch (error) {
     console.log(error)
@@ -29,22 +33,45 @@ async function createUser({ username, password }) {
 
 // why did I need a console.log(error) --> ask allie
 
-async function getUser({ username, password }) {
+
+const getUserByUsername = async (userName) => {
+
+  try {
+    const { rows: [user] } = await client.query(`
+        SELECT * 
+        FROM users
+        WHERE username=$1
+        `, [userName]);
+
+    return user;
+  }
+  catch (error) {
+    console.log(error);
+    throw error;
+  }
+
+}
+
+
+
+const getUser = async ({ username, password }) => {
 
   try {
 
-    const user = await getUserByUserName(username);
+    const user = await getUserByUsername(username);
     const hashedPassword = user.password;
     // isValid will be a boolean based on wether the password matches the hashed password
     const isValid = await bcrypt.compare(password, hashedPassword)
 
     if (isValid) {
       console.log('Passwords match!');
+      delete user.password;
+      return user;
     } else {
       console.log('Passwords don\'t match');
     }
 
-  } 
+  }
   catch (error) {
     console.log(error);
     throw error;
@@ -53,21 +80,17 @@ async function getUser({ username, password }) {
 }
 
 
-async function getUserById(userId) {
-
-}
-
-async function getUserByUsername(userName) {
+const getUserById = async (userId) => {
+  console.log(userId)
 
   try {
-    const {rows: [user] } = await client.query(`
-        SELECT * 
-        FROM users
-        WHERE username=$1
-        RETURNING *;
-        `, [userName]);
-
-        return user;
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE id=${userId};
+    `)
+    delete user.password; 
+    return user;
   }
   catch (error) {
     console.log(error);
@@ -75,6 +98,8 @@ async function getUserByUsername(userName) {
   }
 
 }
+
+
 
 module.exports = {
   createUser,
