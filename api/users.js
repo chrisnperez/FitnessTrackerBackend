dc6ff67f01e152095c7732b579e50a1940a56454
Chris Perez <chrisnaperez@gmail.com>
2023-06-07 21:09:48 -0700

    fix git conflict

--- api/users.js
+++ api/users.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
-<<<<<<< HEAD
 const { getUserByUsername, createUser } = require("../db/users");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
@@ -44,48 +43,6 @@ router.post('/register', async(req, res, next) =>{
     next({name, message})
   }
 });
-=======
-const usersRouter = express.Router();
-
-const { getUserByUsername, createUser } = require('../db');
-
-
-// POST /api/users/register
-usersRouter.post('/register', async (req, res, next) => {
-    const { username, password } = req.body;
-  
-    try {
-      const _user = await getUserByUsername(username);
-  
-      if (_user) {
-        next({
-          name: 'UserExistsError',
-          message: 'A user by that username already exists'
-        });
-      }
-  
-      const user = await createUser({
-        username,
-        password,
-      });
-  
-      const token = jwt.sign({
-        id: user.id,
-        username
-      }, process.env.JWT_SECRET, {
-        expiresIn: '1w'
-      });
-  
-      res.send({
-        message: "thank you for signing up",
-        token
-      });
-    } catch ({ name, message }) {
-      next({ name, message })
-    }
-  });
-  
->>>>>>> 4d2384c (add error handlers)
 // POST /api/users/login
 
 // GET /api/users/me
