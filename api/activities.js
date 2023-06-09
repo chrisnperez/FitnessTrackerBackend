969e203e5aea275de0b23d1b0d8412f7c606e699
Chris Perez <chrisnaperez@gmail.com>
2023-06-08 21:15:19 -0700

    passed get all activities api

--- api/activities.js
+++ api/activities.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const { getAllActivities } = require('../db')
 
-// GET /api/activities/:activityId/routines
+router.use((req, res, next) => {
+    console.log("A request has been made to /activities");
+
+    next();
+})
+
 
 // GET /api/activities
 
+router.get("/", async (req, res, next) => {
+    try{
+        const activities = await getAllActivities();
+        
+        res.send(activities)
+
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+})
+
+
 // POST /api/activities
+
+router.post("/", async (req, res, next) => {
+    try{
+        const activities = await getAllActivities();
+        
+        res.send(activities)
+
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+})
+
+// GET /api/activities/:activityId/routines
 
 // PATCH /api/activities/:activityId
 
