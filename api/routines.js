01ed49b527fedcc5702c18b4bdd2b8e01d577f61
Chris Perez <chrisnaperez@gmail.com>
2023-06-11 15:22:49 -0700

    passed two tests with routines ap

--- api/routines.js
+++ api/routines.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const routinesRouter = express.Router();
+const router = express.Router();
 const { getAllPublicRoutines, createRoutine, updateRoutine, destroyRoutine, getRoutineById, addActivityToRoutine, getRoutineActivitiesByRoutine } = require("../db");
 const { requireUser } = require("./utils");
 
-routinesRouter.use((req, res, next) => {
+router.use((req, res, next) => {
     console.log("A request has been made to /routines");
 
     next();
@@ -11,7 +11,7 @@ routinesRouter.use((req, res, next) => {
 
 // GET /api/routines 
 
-routinesRouter.get("/", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
     try {
         const routines = await getAllPublicRoutines();
 
@@ -28,67 +28,32 @@ routinesRouter.get("/", async (req, res, next) => {
 
 // POST /api/routines
 
-routinesRouter.post("/", requireUser, async (req, res, next) => {
-    console.log(req.body)
+router.post("/", requireUser, async (req, res, next) => {
     const { isPublic, name, goal } = req.body;
     const creatorId = req.user.id;
-    console.log(creatorId)
+  
     try {
-        const routine = await createRoutine({ creatorId, isPublic, name, goal });
+        const newRoutine = await createRoutine({
+            creatorId,
+            isPublic,
+            name,
+            goal,
+        });
 
-        if (routine) {
-            res.send(routine);
-        } else {
-            next(error);
-        }
+        // console.log("creatorId", creatorId);
+        // console.log("newRoutine: ", newRoutine);
+
+        res.send(newRoutine);
+
+    } catch ({ name, message }) {
+        next ({ name, message });
     }
-    catch (error) {
-        next(error);
-    }
-})
+});
 
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
 
-routinesRouter.delete("/:routineId", requireUser, async (req, res, next) => {
-    const { routineId } = req.params;
-    let routine = await getRoutineById(req.params.routineId);
-    try {
-        if (req.user.id === routine.creatorId) {
-            const deleteRoutine = await destroyRoutine(routineId);
-            res.send(deleteRoutine);
-        } else {
-            next({
-                message: "You must be the creator of this routine to delete it"
-            })
-        }
-    }
-    catch (error) {
-        next(error);
-    }
-});
-
-
 // POST /api/routines/:routineId/activities
 
-// router.post("/:routineId/activities", requireUser, async (req, res, next) => {
-//     const routineId = req.params.routineId;
-//     const { activityId, count, duration } = req.body;
-
-//     try {
-//         const routine = await getRoutineById(routineId);
-//         const activity = await addActivityToRoutine({ routineId, activityId, count, duration });
-
-//         if (req.user.id === routine.creatorId) {
-//             res.send(activity);
-//         } else {
-//             next(error);
-//         }
-//     }
-//     catch (error) {
-//         next(error);
-//     }
-// });
-
-module.exports = routinesRouter;
+module.exports = router;
